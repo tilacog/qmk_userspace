c4d1d559fb76e24ef22d3b018c9dbcdea5db9fbd
tilacog <tilacog@gmail.com>
2024-04-30 08:16:21 -0300

    workman layout

---
+++ keyboards/handwired/void_ergo/keymaps/tilacog/keymap.c
@@ -0,0 +1,86 @@
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+    _BASE,
+    _SYMBOL,
+    _FUN,
+    _NUMPAD
+};
+
+// Layer access
+#define ENT_NMP LT(_NUMPAD, KC_ENT)
+#define ENT_FUN LT(_FUN, KC_ENT)
+#define SPC_SYM LT(_SYMBOL, KC_SPC)
+
+// Home row mods - left
+#define HR_LGI LGUI_T(KC_A)
+#define HR_LAL LALT_T(KC_S)
+#define HR_LSH LSFT_T(KC_H)
+#define HR_LCT LCTL_T(KC_T)
+
+// Home row mods - right
+#define HR_RCT LCTL_T(KC_N)
+#define HR_RSH LSFT_T(KC_E)
+#define HR_RAL LALT_T(KC_O)
+#define HR_RGI LGUI_T(KC_I)
+
+// Alt-gr mod tap
+#define LALTGR RALT_T(KC_C)
+#define RALTGR RALT_T(KC_L)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT_split_3x6_5(
+        QK_GESC,  KC_Q  ,  KC_D  , KC_R   , KC_W   ,  KC_B  ,           KC_J   , KC_F   ,  KC_U   , KC_P  , KC_SCLN, _______,
+        KC_BSPC,  HR_LGI,  HR_LAL, HR_LSH , HR_LCT ,  KC_G  ,           KC_Y   , HR_RCT ,  HR_RSH , HR_RAL, HR_RGI , KC_QUOTE,
+        _______,  KC_Z  ,  KC_X  , KC_M   , LALTGR ,  KC_V  ,           KC_K   , RALTGR ,  KC_COMM, KC_DOT, KC_SLSH, _______,
+                    OSM(MOD_HYPR), KC_TAB , SPC_SYM,  ENT_FUN,          ENT_NMP, SPC_SYM,  KC_PGUP, KC_PGDN
+
+    ),
+
+    [_SYMBOL] = LAYOUT_split_3x6_5(
+        _______, _______, KC_BSLS, S(KC_3), S(KC_BSLS), _______,       _______ , S(KC_1)    , S(KC_4), S(KC_5)   , _______, _______,
+        _______, KC_PAST, KC_PSLS, S(KC_2), KC_PEQL,    _______,       _______ , S(KC_MINS) , S(KC_9), S(KC_LBRC), KC_LBRC, _______,
+        _______, KC_PPLS, KC_PMNS, S(KC_6), S(KC_7),    _______,       KC_GRAVE, S(KC_GRAVE), S(KC_0), S(KC_RBRC), KC_RBRC, _______,
+                          _______, _______, _______,    _______,       _______ , _______    , _______, _______
+    ),
+
+    [_FUN] = LAYOUT_split_3x6_5(
+        _______, _______, KC_PGDN, KC_UP  , KC_PGUP , _______,         KC_F12 , KC_F7  , KC_F8  , KC_F9  , _______, KC_CAPS,
+        KC_DEL , _______, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TAB ,         KC_F11 , KC_F4  , KC_F5  , KC_F6  , _______, KC_PSCR,
+        _______, _______, KC_HOME, _______, KC_END  , _______,         KC_F10 , KC_F1  , KC_F2  , KC_F3  , _______, KC_INS ,
+                          _______, _______, _______ , _______,         _______, _______, _______, _______
+    ),
+
+    [_NUMPAD] = LAYOUT_split_3x6_5(
+        _______, _______, _______, _______, _______, _______,          KC_PCMM, KC_7   , KC_8   , KC_9   , _______, _______,
+        _______, _______, _______, _______, _______, _______,          KC_PDOT, KC_4   , KC_5   , KC_6   , _______, _______,
+        _______, _______, _______, _______, _______, _______,          KC_0   , KC_1   , KC_2   , KC_3   , _______, _______,
+                          _______, _______, _______, _______,          _______, _______, KC_0   , KC_0
+    )
+
+    /*
+    [_NEXT] = LAYOUT_split_3x6_5(
+        _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+                          _______, _______, _______, _______,          _______, _______, _______, _______
+    )
+    */
+};
+
+
+/*
+ * ROTARY ENCODER
+ */
+
+#if defined(ENCODER_MAP_ENABLE)
+#define VOLUME ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+#define MOUSE_WHEEL ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [_BASE]   = {VOLUME, MOUSE_WHEEL},
+    [_SYMBOL] = {VOLUME, MOUSE_WHEEL},
+    [_NUMPAD] = {VOLUME, MOUSE_WHEEL},
+    [_FUN]    = {VOLUME, MOUSE_WHEEL}
+};
+#endif
---
+++ script.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# To swich to the correct bootloader, via ISP, use the following command
+# $ avrdude -C /etc/avrdude.conf -c stk500v1 -P /dev/ttyACM0 -p m32u4 -b19200 -B 125kHz -U flash:w:bootloader_atmega32u4_1.0.0.hex:i -U lfuse:w:0x5E:m -U hfuse:w:0xD9:m -U efuse:w:0xF3:m
+
+# Backup existing .hex files by appending a timestamp suffix
+backup_hex_files() {
+    local backup_suffix
+    backup_suffix="$(date +%Y%m%d%H%M%S)"
+    for file in *.hex; do
+        if [ -f "$file" ]; then
+            mv "$file" "${file}_${backup_suffix}" # Rename file with timestamp
+        fi
+    done
+}
+
+# Compile firmware with QMK
+compile_firmware() {
+    qmk compile --clean -kb handwired/void_ergo -km tilacog
+}
+
+# Prepare EEPROM reset file
+prepare_eeprom_reset() {
+    local eeprom_reset_file
+    eeprom_reset_file=$(mktemp)
+    cat <<EOF >"$eeprom_reset_file"
+:0100000000FF
+:00000001FF
+EOF
+    echo "$eeprom_reset_file"
+}
+
+# Wait for user confirmation to continue
+wait_for_user_input() {
+    echo "RESET the device and then press ENTER to continue with flashing the firmware..."
+    read -r  # Wait for user to press enter
+    sleep .75
+}
+
+# Flash the device with the firmware and EEPROM reset file
+flash_firmware() {
+    local eeprom_reset_file=$1
+    dfu-programmer atmega32u4 erase --force
+    dfu-programmer atmega32u4 flash --force --eeprom "$eeprom_reset_file" || true
+    dfu-programmer atmega32u4 flash --force handwired_void_ergo_tilacog.hex
+    dfu-programmer atmega32u4 reset
+}
+
+main() {
+    backup_hex_files
+    compile_firmware
+    eeprom_file=$(prepare_eeprom_reset)
+    wait_for_user_input
+    flash_firmware "$eeprom_file"
+}
+
+main
