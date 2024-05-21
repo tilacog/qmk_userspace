066d1ba6b49140c496100e7f9e82b462b506a818
tilacog <tilacog@gmail.com>
2024-05-21 13:13:11 -0300

    no transparent keys

--- keyboards/handwired/void_ergo/keymaps/tilacog/keymap.c
+++ keyboards/handwired/void_ergo/keymaps/tilacog/keymap.c
@@ -33,29 +33,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC,  KC_Q  ,  KC_D  , KC_R   , KC_W   , KC_B   ,         KC_J   , KC_F   ,  KC_U   , KC_P  , KC_SCLN, _______,
         KC_BSPC,  HR_LGI,  HR_LAL, HR_LSH , HR_LCT , KC_G   ,         KC_Y   , HR_RCT ,  HR_RSH , HR_RAL, HR_RGI , KC_QUOTE,
         _______,  KC_Z  ,  KC_X  , KC_M   , LALTGR , KC_V   ,         KC_K   , RALTGR ,  KC_COMM, KC_DOT, KC_SLSH, _______,
-                    OSM(MOD_HYPR), KC_TAB , ENT_FUN, SPC_SYM,         SPC_SYM, ENT_NMP,  KC_PGUP, KC_PGDN
+                    OSM(MOD_HYPR), KC_TAB , SPC_SYM, ENT_FUN,         ENT_NMP, SPC_SYM,  KC_PGUP, KC_PGDN
 
     ),
 
     [_SYMBOL] = LAYOUT_split_3x6_5(
-        _______, _______, KC_BSLS, S(KC_3), S(KC_BSLS), _______,       _______ , S(KC_1)    , S(KC_4), S(KC_5)   , _______, _______,
-        _______, KC_PAST, KC_PSLS, S(KC_2), KC_PEQL,    _______,       _______ , S(KC_MINS) , S(KC_9), S(KC_LBRC), KC_LBRC, _______,
-        _______, KC_PPLS, KC_PMNS, S(KC_6), S(KC_7),    _______,       KC_GRAVE, S(KC_GRAVE), S(KC_0), S(KC_RBRC), KC_RBRC, _______,
-                          _______, _______, _______,    _______,       _______ , _______    , _______, _______
+        XXXXXXX, XXXXXXX, KC_BSLS, S(KC_3), S(KC_BSLS), XXXXXXX,       XXXXXXX , S(KC_1)    , S(KC_4), S(KC_5)   , XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_PAST, KC_PSLS, S(KC_2), KC_EQL ,    XXXXXXX,       XXXXXXX , S(KC_MINS) , S(KC_9), S(KC_LBRC), KC_LBRC, XXXXXXX,
+        XXXXXXX, KC_PPLS, KC_PMNS, S(KC_6), S(KC_7),    XXXXXXX,       KC_GRAVE, S(KC_GRAVE), S(KC_0), S(KC_RBRC), KC_RBRC, XXXXXXX,
+                          XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,       XXXXXXX , XXXXXXX    , XXXXXXX, XXXXXXX
     ),
 
     [_FUN] = LAYOUT_split_3x6_5(
-        _______, _______, KC_PGDN, KC_UP  , KC_PGUP , _______,         KC_F12 , KC_F7  , KC_F8  , KC_F9  , _______, KC_CAPS,
-        KC_DEL , _______, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TAB ,         KC_F11 , KC_F4  , KC_F5  , KC_F6  , _______, KC_PSCR,
-        _______, _______, KC_HOME, _______, KC_END  , _______,         KC_F10 , KC_F1  , KC_F2  , KC_F3  , _______, KC_INS ,
-                          _______, _______, _______ , _______,         _______, _______, _______, _______
+        XXXXXXX, XXXXXXX, KC_PGDN, KC_UP  , KC_PGUP , XXXXXXX,         KC_F12 , KC_F7  , KC_F8  , KC_F9  , XXXXXXX, KC_CAPS,
+        KC_DEL , XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TAB ,         KC_F11 , KC_F4  , KC_F5  , KC_F6  , XXXXXXX, KC_PSCR,
+        XXXXXXX, XXXXXXX, KC_HOME, XXXXXXX, KC_END  , XXXXXXX,         KC_F10 , KC_F1  , KC_F2  , KC_F3  , XXXXXXX, KC_INS ,
+                          XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
     [_NUMPAD] = LAYOUT_split_3x6_5(
-        _______, _______, _______, _______, _______, _______,          KC_PCMM, KC_7   , KC_8   , KC_9   , _______, _______,
-        _______, _______, _______, _______, _______, _______,          KC_PDOT, KC_4   , KC_5   , KC_6   , _______, _______,
-        _______, _______, _______, _______, _______, _______,          KC_0   , KC_1   , KC_2   , KC_3   , _______, _______,
-                          _______, _______, _______, _______,          _______, _______, KC_0   , KC_0
+        _______, _______, _______, _______, _______, _______,          KC_COMM, KC_7   , KC_8   , KC_9   , XXXXXXX, XXXXXXX,
+        _______, _______, _______, _______, _______, _______,          KC_DOT , KC_4   , KC_5   , KC_6   , XXXXXXX, XXXXXXX,
+        _______, _______, _______, _______, _______, _______,          KC_0   , KC_1   , KC_2   , KC_3   , XXXXXXX, XXXXXXX,
+                          _______, _______, _______, _______,          XXXXXXX, XXXXXXX, KC_0   , KC_0
     )
 
     /*
